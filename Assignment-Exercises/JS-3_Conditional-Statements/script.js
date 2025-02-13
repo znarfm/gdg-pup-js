5a1ccfaab742f62c5950c53ff0edc5826de79906
znarfm <44516102+znarfm@users.noreply.github.com>
2025-02-13 12:29:44 +0800

    finished js3

--- Assignment-Exercises/JS-3_Conditional-Statements/script.js
+++ Assignment-Exercises/JS-3_Conditional-Statements/script.js
@@ -1,3 +1,15 @@
 function checkAge() {
-    // write your code here 
+  let ageFromInput = document.getElementById("ageInput").value;
+  let age = parseInt(ageFromInput);
+
+  let category =
+    age >= 0 && age <= 12 // Check if age is between 0 and 12
+      ? "Child"
+      : age < 20 && age > 12 // Check if age is between 13 and 19
+      ? "Teen"
+      : age >= 20 // Check if age is 20 or above
+      ? "Adult"
+      : "Invalid age"; // If age is negative value or invalid value
+
+  document.getElementById("result").innerHTML = category;
 }

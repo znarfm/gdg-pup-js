2bedae07aca1942631ff214784294d84a4a0220b
znarfm <44516102+znarfm@users.noreply.github.com>
2025-02-15 23:41:46 +0800

    finished js8

--- Assignment-Exercises/JS-8_DOM-Manipulation/JS8.js
+++ Assignment-Exercises/JS-8_DOM-Manipulation/JS8.js
@@ -1,1 +1,19 @@
 // your code starts here
+function incrementCount() {
+  counter++;
+  counterText.textContent = counter;
+}
+
+function decrementCount() {
+  if (counter > 0) counter--;
+  counterText.textContent = counter;
+}
+
+const counterText = document.querySelector("#counter-text");
+const addBtn = document.querySelector("#button-add");
+const minusBtn = document.querySelector("#button-subtract");
+
+let counter = 0;
+
+addBtn.addEventListener("click", incrementCount);
+minusBtn.addEventListener("click", decrementCount);
